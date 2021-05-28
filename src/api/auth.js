15ff542da284e0a2482b7b962bd714946a741c2b
codrutadudau <codrutandudau@gmail.com>
2021-05-28 23:37:59 +0300

    wip login

---
+++ src/api/auth.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = process.env.API_URL;
+
+export const signIn = (email, password) => {
+    return axios
+        .post(API_URL + "/login", { email, password })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+export const signUp = () => {
+    // return axios
+    //     .post(API_URL + "/login", { username, password })
+    //     .then(function (response) {
+    //         console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+}
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,24 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button, Card } from 'react-bootstrap' 
 import { Link } from 'react-router-dom';
+import { authApi } from '../api';
 
 export default function Signin() {
-    const emailRef = useRef();
-    const passwordRef = useRef();
+    const [credentials, setCredentials] = useState({ 
+        email: "",
+        password: ""
+    });
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        authApi.signIn(credentials.email, credentials.password);
+    }
+    
     return (
         <Container className="d-flex align-items-center justify-content-center auth">
             <div className="w-100 auth-inner">
                 <Card>
                     <Card.Body>
                         <h2 className="text-center mb-4">Sign in</h2>
-                        <Form>
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group id="email">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" ref={emailRef} required></Form.Control>
+                                <Form.Control type="email" id="email" onChange={ e => setCredentials({ ...credentials, email: e.target.value })} value={credentials.email} required></Form.Control>
                             </Form.Group>
                             <Form.Group id="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                                <Form.Control type="password" onChange={ e => setCredentials({ ...credentials, password: e.target.value })} value={credentials.password} required></Form.Control>
                             </Form.Group>
                             <Button type="submit" className="w-100 mt-3 auth-inner-submit">Sign in</Button>
                         </Form>
