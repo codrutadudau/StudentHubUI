c3b20b74e8b58ffc54d853d2fef6037a04c6a35f
codrutadudau <codrutandudau@gmail.com>
2021-05-29 10:36:21 +0300

    installed and configured redux-persist

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from './reducers/rootReducer';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
 const store = createStore(
-  rootReducer,
-  composeWithDevTools()
+    persistReducer(persistConfig, rootReducer),
+    composeWithDevTools()
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-  </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <PersistGate persistor={persistStore(store)}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root')
 );
