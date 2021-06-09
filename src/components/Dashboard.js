5680a444bfda490f042449e08c27db0c800d4b79
Codruta Dudau <codrutandudau@gmail.com>
2021-06-09 12:01:31 +0300

    navigation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,70 @@
-import React, { useEffect } from 'react';
-import QuizDashboard from './Quiz/Dashboard';
-import QuestionDashboard from './Question/Dashboard';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import map from 'lodash/map';
+import { Link } from 'react-router-dom';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+
+import '../assets/scss/dashboard.scss';
 
 export default function Dashboard() {
+
+    const navbarItems = () => {
+        return ([
+            {
+                title: 'Home',
+                path: '/dashboard',
+                icon: <HomeIcon />,
+                cName: 'nav-text'
+            },
+            {
+                title: 'Users',
+                path: '/users',
+                icon: <PeopleIcon />,
+                cName: 'nav-text'
+            },
+            {
+                title: 'Quizzes',
+                path: '/quizzes',
+                icon: <FormatListBulletedIcon />,
+                cName: 'nav-text'
+            },
+            {
+                title: 'Questions',
+                path: '/questions',
+                icon: <QuestionAnswerIcon />,
+                cName: 'nav-text'
+            },
+        ]);
+    }
+
     return (
-        <div>
-            {'dashboard'}
-        </div>
+        <>
+            <nav className='navbar-left'>
+                <ul className='nav-menu-items'>
+                    {
+                        map(navbarItems(), item => {
+                            return (
+                                <li className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </nav>
+            <div className="page-content">
+                <Container className="d-flex justify-content-center dashboard">
+                </Container>
+            </div>
+            <div className="navbar-right">
+
+            </div>
+        </>
     );
 }
