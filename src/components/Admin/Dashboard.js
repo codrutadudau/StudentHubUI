7cc56f49bf090a4de453656ee1994f7e20d35909
codrutadudau <codrutandudau@gmail.com>
2021-06-24 01:10:16 +0300

    courses + students for dashboard admin

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import ChartistGraph from 'react-chartist';
+import { useSelector, useDispatch } from 'react-redux';
 import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
 import LocalLibraryOutlinedIcon from '@material-ui/icons/LocalLibraryOutlined';
+import map from 'lodash/map';
+
+import { getAllUsers } from '../../actions/user';
+import { getAllCourses } from '../../actions/course';
 
 import '../../assets/scss/dashboard.scss';
 
 export default function Dashboard() {
+    const dispatch = useDispatch();
+    const [lastRegisteredUsers, setLastRegisteredUsers] = useState();
+    const [lastAddedCourses, setLastAddedCourses] = useState();
+    
+    const users = useSelector(state => state.userReducer.users);
+    const courses = useSelector(state => state.courseReducer.courses);
+
+    useEffect(() => {
+        dispatch(getAllUsers());
+        dispatch(getAllCourses());
+    }, []);
+
+    useEffect(() => {
+        if (users) {
+            setLastRegisteredUsers(users.reverse());
+        }
+    }, [users]);
+
+    useEffect(() => {
+        if (courses) {
+            setLastAddedCourses(courses.reverse());
+        }
+    }, [courses]);
 
     const data = {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
@@ -30,11 +58,35 @@ export default function Dashboard() {
                     <div className="dashboard-grid-item-tag">
                         <PersonAddOutlinedIcon className="dashboard-grid-item-tag-icon" />
                     </div>
+                    <div className="dashboard-grid-item-registered-users">
+                        {
+                            lastRegisteredUsers &&
+                            map(lastRegisteredUsers, (user, index) => {
+                                if (index < 5) {
+                                    return (
+                                        <p key={index} className="dashboard-grid-item-registered-users-item">{user.firstName} {user.lastName}</p>
+                                    )
+                                }
+                            })
+                        }
+                    </div>
                 </div>
                 <div className="dashboard-grid-item">
                     <h4 className="dashboard-grid-item-title">Courses Added</h4>
                     <div className="dashboard-grid-item-tag">
                         <LocalLibraryOutlinedIcon className="dashboard-grid-item-tag-icon" />
+                    </div>
+                    <div className="dashboard-grid-item-added-courses">
+                        {
+                            lastAddedCourses &&
+                            map(lastAddedCourses, (course, index) => {
+                                if (index < 5) {
+                                    return (
+                                        <p key={index} className="dashboard-grid-item-added-courses-item">{course.name}</p>
+                                    )
+                                }
+                            })
+                        }
                     </div>
                 </div>
             </div>
