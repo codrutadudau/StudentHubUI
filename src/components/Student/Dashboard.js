88ccff5ab4d4711efd67202d497022883c43a01e
codrutadudau <codrutandudau@gmail.com>
2021-06-24 00:42:59 +0300

    styling student dashboard

--- src/components/Student/Dashboard.js
+++ src/components/Student/Dashboard.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import map from 'lodash/map';
+import has from 'lodash/has';
 
 import { getQuizInstancesByUser } from '../../actions/quizInstance';
 
 import '../../assets/scss/student.scss';
-import { map } from 'lodash';
 
 export default function Dashboard() {
     const dispatch = useDispatch();
+    const [quizzesByCourse, setQuizzesByCourse] = useState();
 
     const me = useSelector(state => state.userReducer.me);
     const quizzes = useSelector(state => state.quizInstanceReducer.quizzes);
@@ -20,22 +22,48 @@ export default function Dashboard() {
         }
     }, [me]);
 
+    useEffect(() => {
+        if (quizzes) {
+            let groupedQuizzes = {};
+            map(quizzes, quiz => {
+                if (!has(groupedQuizzes, quiz.quiz.course.name)) {
+                    groupedQuizzes[quiz.quiz.course.name] = [];
+                }
+                
+                groupedQuizzes[quiz.quiz.course.name].push(quiz);
+            });
+
+            setQuizzesByCourse(groupedQuizzes);
+        }
+    }, [quizzes]);
+
     return (
         <Container className="d-flex justify-content-center dashboard">
-            Student dashboard
-            <div className="dashboard-course-1">
-                {
-                    quizzes &&
-                    map(quizzes, (quiz, index) =>{
+            <h2 className="dashboard-title">Student dashboard</h2>
+            {
+                quizzesByCourse &&
+                <div className="dashboard-courses">
+                    {
+                        map(quizzesByCourse, (quiz, courseName) =>{
                         return (
-                            <div>
-                                <p className="dashboard-course-1-quiz-name">{quiz.quiz.name}</p>
-                                <LinearProgress variant="determinate" value={quiz.grade * 10} />
+                            <div key={courseName} className="dashboard-courses-item">
+                                <h5 className="dashboard-courses-item-course-name">{courseName}</h5>
+                                {
+                                    map(quiz, (quiz, index) => {
+                                        return (
+                                            <div key={index}>
+                                                <p className="dashboard-courses-item-quiz-name">{quiz.quiz.name}</p>
+                                                <LinearProgress className="dashboard-courses-item-progressbar" variant="determinate" value={quiz.grade * 10} />
+                                            </div>
+                                        );
+                                    })
+                                }
                             </div>
                         );
                     })
-                }
-            </div>
+                    }
+                </div>
+            }
         </Container>
     );
 }
